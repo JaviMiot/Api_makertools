a5d30289159ad162390113e770ae8d5f8e6c2b38
Javier Manobanda <51manobandajavier@gmail.com>
2021-09-17 15:10:40 -0500

    add flask_cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .config import CONFIGS
-
+from flask_cors import CORS
 # * Blueprints
 
 from app.printer import printer_App
@@ -9,6 +9,7 @@ from app.laserCut import laserCut_App
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(CONFIGS['default'])
     app.register_blueprint(laserCut_App)
     app.register_blueprint(printer_App)
